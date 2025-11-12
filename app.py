2b84a7fb5d48b33cf346d793193682b78bf2827d
abdullahShahzad-512 <bsef23a032@pucit.edu.pk>
2025-11-12 09:50:31 +0500

    update message

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 def main():
     print("Hello from Jenkins CI/CD with Python!")
+    print("Hello, this is another line to check automatically update from Jenkins CI/CD with Python!")
+
 
 if __name__ == "__main__":
     main()
