f3e714310b168917acc8a4c18009f3bc3ebf85c8
abdullahShahzad-512 <bsef23a032@pucit.edu.pk>
2025-11-23 23:21:56 +0500

    UPDATE FOR LAB4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello from Jenkins CI/CD with Python!")
     print("Hello, this is another line to check automatically update from Jenkins CI/CD with Python!")
+    print("Hello, this is new line for lab 4 to check, Jenkins will automatically build  project.")
 
 
 if __name__ == "__main__":
